5831f8b1cd671cfc2f2c09a8872aa20d704bc176
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2019-06-25 22:26:35 +0200

    Migrated from polling to webhooking

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+# [START gae_python37_app]
 import logging
 import os
-import json
 
-from flask import Flask
+from flask import Flask, request
 from telegram import Update
 from telegram.ext import Updater, Dispatcher
 
@@ -15,11 +15,10 @@ logging.basicConfig(format='%(message)s',
 logger = logging.getLogger('cunhaobot')
 TG_TOKEN = os.environ["TG_TOKEN"]
 BASE_URL = os.environ["BASE_URL"]
-PORT = os.environ["PORT"]
+PORT = os.environ.get("PORT")
 
 
 def tg_dispatcher() -> Dispatcher:
-    logger.info("STARTING MAIN")
     # Create the Updater and pass it your bot's token.
     updater = Updater(TG_TOKEN)
 
@@ -32,32 +31,29 @@ def tg_dispatcher() -> Dispatcher:
     # log all errors
     dp.add_error_handler(error_handler)
 
-    # Start the Bot
-    # updater.bot.set_webhook(f'{BASE_URL}:{PORT}/{TG_TOKEN}')
-    print(f'{BASE_URL}/{TG_TOKEN}')
-
     return dp
 
 
 app = Flask(__name__)
 
-# Not pretty, but I needed some sort of http server to work in appengine standard
+
 @app.route('/')
-def ping(self):
+def ping():
     return "I am alive"
 
-#class TelegramHandler(webapp2.RequestHandler):
-#    def post(self):
-#        dispatcher = tg_dispatcher()
-#        update = Update.de_json(json.loads(self.request.body), dispatcher.bot)
-#        dispatcher.process_update(update)
 
-
-print("SETTING APP")
+@app.route(f'/{TG_TOKEN}', methods=['POST'])
+def telegram_handler():
+    dispatcher = tg_dispatcher()
+    update = Update.de_json(request.json, dispatcher.bot)
+    dispatcher.process_update(update)
+    return "Handled"
 
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
+
+# [END gae_python37_app]
