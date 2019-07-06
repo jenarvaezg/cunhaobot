7c8a236c0cb7be0a47837a6ff2173e4b329e422d
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2019-07-06 14:03:02 +0200

    Add small script to resync datastore keys after manual udpate

---
+++ scripts/sync_datastore_keys.py
@@ -0,0 +1,16 @@
+from google.cloud import datastore
+
+
+datastore_client = datastore.Client()
+kind = "Phrase"
+query = datastore_client.query(kind=kind)
+changes = []
+for entity in query.fetch():
+    old_key = entity.key
+    key_from_text = datastore_client.key(kind, entity['text'])
+    if old_key != key_from_text:
+        print(key_from_text, old_key)
+        changes.append(entity)
+        datastore_client.delete(old_key)
+        entity.key = key_from_text
+        datastore_client.put(entity)
