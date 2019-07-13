867a06307f19b30ca4b465e2e7361de4152c2447
José Enrique Narváez Gago <jenarvaezg@gmail.com>
2019-07-13 22:14:14 +0200

    Small tweaks to fuzz

--- tg/handlers/submit.py
+++ tg/handlers/submit.py
@@ -13,7 +13,7 @@ from tg.utils.user import user_from_update
 
 curators_chat_id = os.environ.get("MOD_CHAT_ID", "")
 SIMILARITY_DISCARD_THRESHOLD = int(os.getenv("PHRASE_DISMISSAL_SIMILARITY_THRESHOLD", 90))
-SIMILARITY_WARNING_THRESHOLD = int(os.getenv("PHRASE_WARNING_SIMILARITY_THRESHOLD", 70))
+SIMILARITY_WARNING_THRESHOLD = int(os.getenv("PHRASE_WARNING_SIMILARITY_THRESHOLD", 50))
 
 proposal_t = Union[Type[Proposal], Type[LongProposal]]
 phrase_t = Union[Type[Phrase], Type[LongPhrase]]
@@ -37,11 +37,13 @@ def submit_handling(bot: Bot, update: Update, proposal_class: proposal_t, phrase
         similarity_ratio = fuzz.ratio(lowercase_proposal, phrase.lower())
         if SIMILARITY_DISCARD_THRESHOLD < similarity_ratio:
             return update.effective_message.reply_text(
-                f'Esa ya la tengo, {Phrase.get_random_phrase()}, {Phrase.get_random_phrase()}. Se parece a {phrase}, {Phrase.get_random_phrase()}')
+                f'Esa ya la tengo, {Phrase.get_random_phrase()}, {Phrase.get_random_phrase()}. '
+                f'Se parece demasiado a "<b>{phrase}</b>", {Phrase.get_random_phrase()}.',
+                parse_mode=ParseMode.HTML,
+            )
         if SIMILARITY_WARNING_THRESHOLD < similarity_ratio:
             if warning_phrase is None or warning_phrase[0] < similarity_ratio:
                 warning_phrase = (similarity_ratio, phrase)
-
 
     proposal.save()
 
@@ -50,6 +52,7 @@ def submit_handling(bot: Bot, update: Update, proposal_class: proposal_t, phrase
                             f"\n'<b>{proposal.text}</b>'"
     if warning_phrase is not None:
         curators_message_text +=f"\nSe parece a '<b>{warning_phrase[1]}</b>' ({warning_phrase[0]}%)"
+
     bot.send_message(curators_chat_id, curators_message_text, reply_markup=curators_reply_markup,
                      parse_mode=ParseMode.HTML)
     update.effective_message.reply_text(
