2e50829ba0cf8713cf34c471a2a31712f89faf95
José Enrique Narváez <jenarvaezg@gmail.com>
2025-12-11 07:46:49 +0100

    F

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 prev_version=$(gcloud app versions list | tail -n 1 | tr -s " " | cut -d' ' -f2)
 
-echo y | gcloud app deploy --stop-previous-version
+echo y | gcloud app deploy --stop-previous-version --promote --no-cache
 echo y | gcloud app versions delete $prev_version
 
 curl https://cunhaobot.appspot.com
